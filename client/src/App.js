9649ec0246ee197c34dccfc994a7f3d173393074
Vishva <vishva090300@gmail.com>
2025-03-06 11:24:47 +0530

    resources setup

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home/Home";
 import Services from "./pages/Services/Services";
+import Resources from "./pages/Resources/Resources";
+
 function App() {
   return (
     <div className="App">
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="services" element={<Services/>}/>
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="services" element={<Services />} />
+          <Route path="/resources" element={<Resources />} />
+
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/pages/Resources/Resources.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../../components/Header/Header'
+
+function Resources() {
+    return (
+        <div>
+            <Header />
+            <div>
+                <p>Resources</p>
+            </div>
+        </div>
+    )
+}
+
+export default Resources
