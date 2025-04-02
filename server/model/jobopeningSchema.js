2f82206e71f18f53998b0ef93f770569f7f48834
Vishva <adharsh.scipy@gmail.com>
2025-04-02 11:25:26 +0530

    job opening

--- server/model/jobopeningSchema.js
+++ server/model/jobopeningSchema.js
@@ -3,18 +3,10 @@ import mongoose, { Schema } from "mongoose";
 const jobopeningSchema = new Schema({
     title: { type: String, required: true },
     location: { type: String, required: true },
-    jobDescription: { type: String, required: true },
+    workSchedule: { type: String, required: true },
     jobType: { type: String, required: true },
+    workTime: { type: String, required: true },
     requiredSkills: [{ type: String }],
-    // responsibilities: [{ type: String }],
-    // qualifications: [{ type: String }],
-    // datePosted: { type: Date, default: Date.now },
-    // contactEmail: { type: String, required: true },
-    // contactPhone: { type: String, required: true },
-    // applicationDeadline: { type: Date },
-    // numberOfPositions: { type: Number, required: true },
-    // salaryRange: { type: String, required: true },
-    // jobStatus: { type: String, default: 'open' }
 })
 
 export const JobOpening = new mongoose.model("JobOpening", jobopeningSchema)
