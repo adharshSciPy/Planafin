49733ca7e412d7c27a7ee8f4a1ebdf3d9536246f
gokul <gokulsunil2015@gmail.com>
2025-04-09 11:22:56 +0530

    eye icon

--- client/src/pages/Admin/Adminlogin.jsx
+++ client/src/pages/Admin/Adminlogin.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import React, { useState, } from "react";
 import baseUrl from "../../baseUrl";
 import { useNavigate} from "react-router-dom";
-import { Eye, Trash } from 'lucide-react';
+import { Eye, EyeOff } from 'lucide-react';
 
 
 import "./adminlogin.css";
@@ -14,6 +14,7 @@ function Adminlogin() {
   };
  const navigate=useNavigate();
   const [form,setForm]=useState(field);
+  const [showPassword,setShowPassword]=useState(false);
   const handleChange = (e) => {
     setForm({
       ...form,
@@ -36,12 +37,15 @@ navigate('/admindashboard')
       
     }
   }
+  const passwordToggle=()=>{
+    setShowPassword(prev => !prev);
+  }
 
   return (
     <div>
        <div className="admlogin-container">
       <h2>Login</h2>
-      <Eye size={25} /> 
+     
 
       <form onSubmit={handleSubmit}>
         <div className="adminput-group">
@@ -57,14 +61,18 @@ navigate('/admindashboard')
 
         <div className="adminput-group">
           <label>Password</label>
+          <div className="input-span-btn23">
           <input
-            type="password"
+           type={showPassword ? 'text' : 'password'}
             name="password"
             placeholder="Enter your password"
             value={form.password}
             onChange={handleChange}
             
           />
+          <span onClick={passwordToggle} className="eyebtn"> {showPassword?<Eye size={25} />:<EyeOff size={25}/>} </span>
+          </div>
+
         </div>
 
         <button className="admbtn" type="submit">Login</button>
