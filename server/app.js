e97a3b14c2cede9c614aac52e82a2f7b0a35b79e
Sanju tl <sanjutl19@gmail.com>
2025-05-20 11:52:20 +0530

    planafin

--- server/app.js
+++ server/app.js
@@ -1,25 +1,30 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv'
-import userRoute from './routes/userRoute.js';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import userRoute from "./routes/userRoute.js";
 import path from "path";
-import { fileURLToPath } from 'url';
-import "./jobs/reminderCorn.js"
+import { fileURLToPath } from "url";
+import "./jobs/reminderCorn.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-dotenv.config()
+dotenv.config();
 
 const app = express();
 // dotenv.config({
 //     path: './env'
 // })
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true, limit: '16kb' }))
-app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(cors());
 
-app.use('/api/v1/user', userRoute)
+app.use("/api/v1/user", userRoute);
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+const buildPath = path.join(__dirname, "../client/build");
+app.use(express.static(buildPath));
 
-export { app,__dirname }
+app.get("*", (req, res) => {
+  res.sendFile(path.join(buildPath, "index.html"));
+});
+export { app, __dirname };
